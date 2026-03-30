5985b095ba6fdae540d80af72c705f553a5d51ac
AldanaMajo <majoaldana178@gmail.com>
2026-03-30 16:59:31 -0600

    Genere  la capa UImvc y agregue controlador + vista de la clase rol

---
+++ ComercialWeb/ComercialWebMVCUI/Controllers/RolController.cs
@@ -0,0 +1,101 @@
+﻿using ComercialWebBL;
+using ComercialWebDAL;
+using ComercialWebEN;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComercialWebMVCUI.Controllers
+{
+   
+    public class RolController : Controller
+    {
+        RolBL rolbl = new RolBL();
+        // GET: RolController
+        public async Task<IActionResult> Index(RolEN pRol = null)
+        {
+            if (pRol == null)
+                pRol = new RolEN();
+            if (pRol.Top_Aux == 0)
+                pRol.Top_Aux = 10;
+            else
+                if (pRol.Top_Aux == -1)
+                    pRol.Top_Aux = 0;
+
+            var roles = await rolbl.BuscarAsync(pRol);
+            ViewBag.Top = pRol.Top_Aux;
+            return View(roles);
+        }
+
+        // GET: RolController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RolController/Create
+        public IActionResult Create()
+        {
+            ViewBag.Error = "";
+            return View();
+        }
+
+        // POST: RolController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RolEN pRol)
+        {
+            try
+            {
+                int result = await rolbl.GuardarAsync(pRol);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(pRol);
+            }
+        }
+
+        // GET: RolController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RolController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RolController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RolController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ComercialWeb/ComercialWebMVCUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComercialWebMVCUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
