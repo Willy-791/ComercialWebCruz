0b1546009ea1d2b2257fb149735d9a33eeac658b
David25-gif <davidmunguia571@gmail.com>
2026-04-07 22:41:58 -0600

    cree las vistas de residencia y su controlador, aun no funciona por problema de la base de datos

---
+++ ComercialWeb/ComercialWebMVCUI/Controllers/ResidenciaController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ComercialWebEN;
+using ComercialWebBL;
+
+public class ResidenciaController : Controller
+{
+    ResidenciaBL residenciaBL = new ResidenciaBL();
+
+    public async Task<IActionResult> Index()
+    {
+        var lista = await residenciaBL.ObtenerTodosAsync();
+        return View(lista);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ResidenciaEN p)
+    {
+        if (ModelState.IsValid)
+        {
+            await residenciaBL.GuardarAsync(p);
+            return RedirectToAction("Index");
+        }
+        return View(p);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var res = await residenciaBL.ObtenerPorIdAsync(new ResidenciaEN { IdResidencia = id });
+        return View(res);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ResidenciaEN p)
+    {
+        await residenciaBL.ModificarAsync(p);
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var res = await residenciaBL.ObtenerPorIdAsync(new ResidenciaEN { IdResidencia = id });
+        return View(res);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(ResidenciaEN p)
+    {
+        await residenciaBL.EliminarAsync(p);
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        var res = await residenciaBL.ObtenerPorIdAsync(new ResidenciaEN { IdResidencia = id });
+        return View(res);
+    }
+}
